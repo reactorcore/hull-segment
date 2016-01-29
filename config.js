eb8917f974917e157de3d37d54e4d661d44e3d8e
Romain Dardour <romain@hull.io>
2016-01-29 15:53:06 +0100

    update tooling

--- config.js
+++ config.js
@@ -1,32 +1,107 @@
-"use strict";
+'use strict';
 
-/*global module, require, process, __dirname */
-var webpack = require("webpack");
-var path = require("path");
-var pkg = require("./package.json");
-var manifest = require("./manifest.json");
-var moment = require("moment");
+/* global module, require, process, __dirname */
+
+var _ = require('lodash');
+var webpack = require('webpack');
+var path = require('path');
+var moment = require('moment');
+
+var pkg = require('./package.json');
+var manifest = require('./manifest.json');
 
 // DO NOT CHANGE FOLDERS
 // WIHTOUT UPDATING PACKAGE.JSON TOO.
-var sourceFolder = "src";
-var outputFolder = "dist";
-var assetsFolder = "";
-var serverPort   = process.env.PORT||8081;
-var previewUrl   = "http://localhost:"+serverPort;
+var sourceFolder = 'src';
+var outputFolder = 'dist';
+var assetsFolder = '';
+var serverPort = process.env.PORT || 8081;
+var previewUrl = 'http://localhost:' + serverPort;
 
-var hotReload = false;
+var hotReload = true;
+
+var libName = pkg.name;
+var displayName = manifest.name || libName;
 
 // DO NOT CHANGE SHIP ENTRY
 // WITHOUT UPDATING PACKAGE.JSON TOO
 // THESE ARE THE JS FILES USED AS ENTRY POINTS TO COMPILE YOUR APP
 
+
+/*
+  --------------------------------
+  Compiled JS files
+  --------------------------------
+*/
 var entry = {
-  ship:  "./"+sourceFolder+"/ship.js"
+  ship: './' + sourceFolder + '/ship.js',
+  index: './' + sourceFolder + '/index.js',
 };
 
-var jsFiles = ["src/**/*.js", "src/**/*.jsx"];
+/*
+  --------------------------------
+  Sketch icons extraction
+  --------------------------------
+*/
+var sketch = {
+  export: 'artboards',
+  formats: 'svg',
+};
 
+/*
+  --------------------------------
+  SVGO Icons Optimization
+  --------------------------------
+*/
+var imagemin = {
+  progressive: true,
+  svgoPlugins: [{
+    removeViewBox: false,
+    convertTransform: true,
+  }],
+};
+
+/*
+  --------------------------------
+  iconfont generation
+  --------------------------------
+*/
+var icons = {
+  folder: path.join(sourceFolder, 'icons'),
+  src: path.join(sourceFolder, 'icons', '**', '*'),
+  fontPath: path.join('../', assetsFolder, 'fonts'),
+
+  output: {
+    sprite: path.join(outputFolder, assetsFolder),
+    font: path.join(outputFolder, assetsFolder, 'fonts'),
+    css: path.join(sourceFolder, 'styles'),
+  },
+
+  sprite: {
+    shape: {
+      dimension: { maxWidth: 32, maxHeight: 32 },
+      spacing: { padding: 0 },
+    },
+    mode: {
+      view: { bust: false, dest: 'sprite', render: { scss: false } },
+    },
+  },
+
+  font: {
+    fontName: libName + 'Font',
+    fontHeight: 1001,
+    normalize: true,
+    formats: ['ttf', 'eot', 'woff', 'svg'],
+    timestamp: Math.round(Date.now() / 1000),
+  },
+};
+
+
+/*
+  --------------------------------
+  Copied files
+  --------------------------------
+*/
 var files = {
   'src/vendors/**/*': path.join(outputFolder, assetsFolder, 'vendors'),
   'src/images/**/*': path.join(outputFolder, assetsFolder, 'images'),
@@ -41,85 +116,166 @@ var files = {
   'CNAME': outputFolder,
 };
 
-var libName = pkg.name;
-var displayName = manifest.name||libName;
-
-
-
-// ------------------------------------------------
-// ------------------------------------------------
-// NO NEED TO TOUCH ANYTHING BELOW THIS
-// ------------------------------------------------
-// ------------------------------------------------
-
-var outputPath = path.join(__dirname, outputFolder);
-
 var output = {
-  path: path.join(outputPath,assetsFolder,"/"),
+  path: path.join(__dirname, outputFolder, assetsFolder, '/'),
   pathinfo: true,
-  filename: "[name].js",
-  chunkFileName: "[name].chunk.js",
-  libraryTarget: "umd",
-  library: 'Hull'+displayName+'Ship',
-  publicPath: assetsFolder+"/"
+  filename: '[name].js',
+  chunkFileName: '[name].chunk.js',
+  publicPath: assetsFolder + '/',
 };
 
-var extensions         = ["", ".js", ".jsx", ".css", ".scss"];
+var resolve = { extensions: ['', '.js', '.jsx', '.css', '.scss'] };
 
-var modulesDirectories = ["node_modules", "bower_components", "bower_components/foundation/scss/", "src/vendor"];
+// SASS-LIKE :
+// postcss-simple-vars
+// postcss-advanced-vars
+// postcss-sassy-mixins
+// postcss-simple-extend
+// postcss-vertical-rhythm
+var postcss = [
+  require('precss')({ /* options */ }), // Sass-like syntax
+  require('postcss-round-subpixels'),
+  /* IE */
+  require('postcss-pseudoelements'),
+  require('postcss-color-rgba-fallback'),
+  require('postcss-opacity'),
+  require('postcss-vmin'),
+  /* END IE */
+  require('postcss-initial'), // all:initial
+  require('cssnext'),
+  require('cssnano')(), // Condense & Optimize
+];
 
-var cssIncludes   = modulesDirectories.map(function(include){
-  return ("includePaths[]="+path.resolve(__dirname, include));
-}).join("&");
+// about babel : it's VERY SLOW. DO NOT APPLY IT TO EVERY SOURCE FILE. see the Excludes we applied
+var loaderLibrary = {
+  json: {test: /\.json$/, loader: 'json' },
+  css: {test: /\.(css|scss)$/, loaders: ['style?singleton=true', 'css?modules&importLoaders=1', 'postcss'] },
+  devCss: {test: /\.(css|scss)$/, loaders: ['style?singleton=true', 'css?modules&importLoaders=1&localIdentName=_[name]__[local]___[hash:base64:5]', 'postcss'] },
+  file: {test: /\.jpe?g$|\.gif$|\.png|\.woff$|\.ttf$|\.wav$|\.mp3$/, loader: 'file' },
+  svg: {test: /\.svg$/, loader: 'svg-inline' },
+  js: {test: /\.(js)$/, loader: 'babel', exclude: /node_modules|src\/vendors/ },
+  prodJSX: {test: /\.(jsx)$/, loader: 'babel' },
+  devJSX: {test: /\.(jsx)$/, loaders: ['react-hot', 'babel'] },
+};
 
+var devLoaders = [
+  loaderLibrary.json,
+  loaderLibrary.devCss,
+  loaderLibrary.file,
+  loaderLibrary.js,
+  (hotReload ? loaderLibrary.devJSX : loaderLibrary.prodJSX)
+];
 
-// https://github.com/webpack/react-starter/blob/master/make-webpack-config.js
-// "imports?define=>false": Yeah, we"re going big and disabling AMD completely. F**k it.
-// This is because webpack strips the `this` context when requiring those, while they expect it.
-// Basically, this fixes all of our problems with badly constructed AMD modules.
-// Among which: vex, datepicker, underscore-contrib
 var loaders = [
-  {test: /\.json$/,                loaders: ["json-loader"] },
-  {test: /\.js$/,                  loaders: ["babel-loader"], exclude: /node_modules|bower_components/},
-  {test: /\.(css|scss)$/,          loaders: ["style/useable", "css-loader", "autoprefixer-loader?browsers=last 2 version", "sass-loader?outputStyle=expanded&"+cssIncludes]}
-] ;
+  loaderLibrary.json,
+  loaderLibrary.css,
+  loaderLibrary.file,
+  loaderLibrary.js,
+  loaderLibrary.prodJSX,
+];
 
-// We remove the "dist" from the filenames for demo and index.html in package.json
+
+// We remove the 'dist' from the filenames for demo and index.html in package.json
 // Package.json expects our files to be addressable from the same repo
 // We put them in `dist` to have a clean structure but then we need to build them in the right place
 var plugins = [
+  new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  // new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'vendors.js', minChunks: Infinity}),
   new webpack.DefinePlugin({
-    "BUILD_DATE" : JSON.stringify(moment().format("MMMM, DD, YYYY, HH:mm:ss")),
-    "PUBLIC_PATH": JSON.stringify(output.publicPath)
+    BUILD_DATE: JSON.stringify(moment().format('MMMM, DD, YYYY, HH:mm:ss')),
+    PUBLIC_PATH: JSON.stringify(output.publicPath),
   }),
-  new webpack.ResolverPlugin(
-    new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
-  ),
-  new webpack.optimize.OccurenceOrderPlugin()
 ];
 
-var externals = {};
+
+/*
+  ----------------------------
+  LOCALTUNNEL
+  ----------------------------
+*/
+var localtunnel = {
+  subdomain: libName.toLowerCase().replace(/[-_]/g, ''),
+};
+
+/*
+  ----------------------------
+  CLOUDFRONT
+  ----------------------------
+*/
+var cloudfront;
+if (process.env.AWS_KEY && process.env.AWS_SECRET) {
+  var cloudfrontInvalidations = ['/' + libName + '/*'];
+  cloudfront = {
+    config: {
+      credentials: {
+        accessKeyId: process.env.AWS_KEY,
+        secretAccessKey: process.env.AWS_SECRET,
+      },
+      distributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
+      region: 'us-east-1',
+    },
+    invalidationBatch: {
+      CallerReference: new Date().toString(),
+      Paths: {
+        Quantity: cloudfrontInvalidations.length,
+        Items: cloudfrontInvalidations,
+      },
+    },
+  };
+}
+
+/*
+  ----------------------------
+  DEV MODE / HOT RELOAD overrides
+*/
+var devPlugins, devEntry;
+if (hotReload) {
+  devEntry = _.reduce(entry, function(entries, v, k) {
+    entries[k] = [ 'webpack-dev-server/client?' + previewUrl, 'webpack/hot/only-dev-server', v ];
+    return entries;
+  }, {});
+  devPlugins = plugins.concat([new webpack.HotModuleReplacementPlugin()]);
+} else {
+  devEntry = entry;
+  devPlugins = plugins;
+}
 
 module.exports = {
-  hotReload          : hotReload,
-  libName            : libName,
-  displayName        : displayName,
 
-  files              : files,
-  jsFiles            : jsFiles,
+  libName: libName,
+  displayName: displayName,
 
-  outputFolder       : outputFolder,
-  assetsFolder       : assetsFolder,
-  serverPort         : serverPort,
-  previewUrl         : previewUrl,
+  hotReload: hotReload,
+  localtunnel: localtunnel,
+  cloudfront: cloudfront,
 
-  entry              : entry,
-  output             : output,
-  extensions         : extensions,
-  modulesDirectories : modulesDirectories,
-  plugins            : plugins,
-  loaders            : loaders,
-  externals          : externals,
+  files: files,
 
-  pkg                : pkg
+  sketch: sketch,
+  imagemin: imagemin,
+  icons: icons,
+
+  sourceFolder: sourceFolder,
+  outputFolder: outputFolder,
+  assetsFolder: assetsFolder,
+  serverPort: serverPort,
+  previewUrl: previewUrl,
+
+  devEntry: devEntry,
+
+  entry: entry,
+  output: output,
+
+  plugins: plugins,
+  devPlugins: devPlugins,
+  devLoaders: devLoaders,
+
+  resolve: resolve,
+  loaders: loaders,
+
+  postcss: postcss,
+
+  pkg: pkg,
+
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,187 +1,51 @@
-"use strict";
-/*global require, process*/
-
-var _                = require("underscore");
-var path             = require("path");
-
-var del              = require("del");
-var runSequence      = require("run-sequence");
-var notifier         = require("node-notifier");
-var ngrok            = require("ngrok");
-var ghPages         = require("gh-pages");
-
-var gulp             = require("gulp");
-var gulpLoadPlugins  = require("gulp-load-plugins");
-var opn              = require('opn')
-var $                = gulpLoadPlugins();
+'use strict';
+/* global require, console*/
 
 
-var webpack          = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var config      = require('./config');
+
+[
+  'clean',
+  'cloudfront',
+  'copy',
+  'deploy',
+  'format',
+  'lint',
+  'localtunnel',
+  'webpack',
+  // 'iconfont',
+  // 'iconsprite',
+  // 'sass',
+  // 'serve',
+  // 'sketch',
+].map(function(task) { require('./gulp_tasks/' + task + '.js')(gulp, config);});
 
 
-// Get our Config.
-var config           = require("./config");
-var webpackConfig    = require("./webpack.config");
+gulp.task('default', ['server']);
 
-
-// Task Bundles
-gulp.task("default", ["server"]);
-gulp.task("serve",   ["server"]);
-
-gulp.task("server",  function(callback) {runSequence("lint:watch", "clean", "copy-files:watch", "webpack:server", callback); });
-gulp.task("build",   function(callback) {runSequence("lint", "clean", "copy-files", "webpack:build", callback); });
-gulp.task("deploy",  function(callback) {runSequence("build", "gh:deploy", callback); });
-
-
-var notify = function(message){
-  notifier.notify({title: config.displayName+" Gulp",message:message});
-};
-
-// Raise errors on Webpack build errors
-var webpackFeedbackHandler = function(err, stats){
-  handleError(err);
-
-  var jsonStats = stats.toJson();
-
-  if(jsonStats.errors.length > 0){
-    $.util.log("[webpack:build:error]", JSON.stringify(jsonStats.errors));
-    throw new $.util.PluginError("webpack:build:error", JSON.stringify(jsonStats.errors));
-  }
-
-  // Don't throw an error here : Uglify uses a lot of warnings to mention stripped code
-  if(jsonStats.warnings.length > 0){
-    $.util.log("[webpack:build:warning]", JSON.stringify(jsonStats.warnings,null,2));
-  }
-};
-
-// Copy static files from the source to the destination
-var copyFiles = function(callback){
-  _.map(config.files,function(dest, src){
-    gulp.src(src).pipe(gulp.dest(dest));
-  });
-  notify("Vendors Updated");
-  if(_.isFunction(callback)) {
-    callback();
-  }
-};
-
-var lint = function(callback){
-  return gulp.src(config.jsFiles)
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-}
-
-// Handle Gulp Errors
-var handleError = function(err, taskName){
-  if(err){
-    notify(taskName+" Error: "+ err);
-    throw new $.util.PluginError("webpack:build", err);
-  }
-};
-
-// Setup a Ngrok server
-var ngrokServe = function(subdomain){
-  var options = { port: config.serverPort };
-  var env = process.env;
-  if (env.NGROK_AUTHTOKEN) {
-    options.authtoken = env.NGROK_AUTHTOKEN;
-  }
-  if(env.NGROK_SUBDOMAIN || subdomain){
-    options.subdomain = env.NGROK_SUBDOMAIN || subdomain;
-  }
-  ngrok.connect(options, function (error, url) {
-    if (error) throw new $.util.PluginError("ship:server", error);
-    url = url.replace("https", "http");
-    notify({message:"Ngrok Started on "+url});
-    $.util.log("[ship:server]", url);
-    opn(url);
-  });
-};
-
-/**
- * GULP TASKS START HERE
-*/
-
-
-// Cleanup build folder
-gulp.task("clean",   function(cb)       {del(["./"+config.outputFolder+"/**/*"], cb); });
-
-// One-time file copy
-gulp.task("copy-files", copyFiles);
-
-// Watch files for changes and copy them
-gulp.task("copy-files:watch", function(){
-  copyFiles();
-  gulp.watch(_.keys(config.files),copyFiles);
+gulp.task('prepare', function(callback) {
+  runSequence('clean', ['copy'], callback);
 });
 
-
-
-
-gulp.task("lint", function() {
-  lint().pipe($.eslint.failAfterError());
-});
-gulp.task("lint:watch", function() {
-  lint();
-  var lintLater = _.debounce(lint, 500);
-  gulp.watch(config.jsFiles, lintLater)
+gulp.task('watch', function(callback) {
+  runSequence(['copy:watch', 'lint:watch'], callback);
 });
 
-//Production Build.
-//Minified, clean code. No demo keys inside.
-//demo.html WILL NOT WORK with this build.
-//
-//Webpack handles CSS/SCSS, JS, and HTML files.
-gulp.task("webpack:build", function(callback) {
-  // Then, use Webpack to bundle all JS and html files to the destination folder
-  notify("Building App");
-  webpack(_.values(webpackConfig.production), function(err, stats) {
-    var feedback = webpackFeedbackHandler(err,stats);
-    $.util.log("[webpack:build]", stats.toString({colors: true}));
-    notify({message:"App Built"});
-    callback(feedback);
-  });
+gulp.task('serve', function(callback) {
+  runSequence(['webpack:server', 'localtunnel'], callback);
 });
 
-// Dev Build
-// Create the webpack compiler here for caching and performance.
-var webpackDevCompiler = webpack(webpackConfig.development.browser);
-
-// Build a Dev version of the project. Launched once on startup so we can have eveything copied.
-gulp.task("webpack:build:dev", function(callback) {
-  // run webpack with Dev profile.
-  // Embeds the Hull config keys, and the necessary stuff to make demo.html work
-  webpackDevCompiler.run(function(err, stats) {
-    var feedback = webpackFeedbackHandler(err,stats);
-    $.util.log("[webpack:build:dev]", stats.toString({colors: true}));
-    notify({message:"Webpack Updated"});
-    callback(feedback);
-  });
+// Batch, Public Tasks
+gulp.task('server', function(callback) {
+  runSequence('prepare', 'watch', 'serve', callback);
 });
 
-// Launch webpack dev server.
-gulp.task("webpack:server", function() {
-  var taskName = "webpack:server";
-  new WebpackDevServer(webpackDevCompiler, {
-    contentBase: config.outputFolder,
-    publicPath: "/"+config.assetsFolder,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    hot: config.hotReload,
-    stats: {colors: true }
-  }).listen(config.serverPort, function(err) {
-    handleError(err, taskName);
-    // Dump the preview URL in the console, and open Chrome when launched for convenience.
-    var url = webpackConfig.development.browser.output.publicPath+"webpack-dev-server/";
-    $.util.log("["+taskName+"] started at ", url);
-    notify({message:"Dev Server Started"});
-    ngrokServe(config.libName);
-  });
+gulp.task('build', function(callback) {
+  runSequence('prepare', 'webpack', callback);
 });
 
-
-// Deploy production bundle to gh-pages.
-gulp.task("gh:deploy", function (callback) {
-  notify("Deploying "+config.outputFolder+" to Github Pages");
-  ghPages.publish(path.join(process.cwd(), config.outputFolder), callback);
+gulp.task('deploy', function(callback) {
+  runSequence('build', 'gh:deploy', 'cloudfront', callback);
 });
