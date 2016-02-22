43400494351f1bc139938820d2c22bafa006c552
Stephane Bellity <sbellity@gmail.com>
2016-02-22 18:11:16 +0100

    segment webhooks handler

---
+++ server/handler.js
@@ -0,0 +1,173 @@
+import connect from 'connect';
+import https from 'https';
+import _ from 'lodash';
+import rawBody from 'raw-body';
+import Hull from 'hull';
+import crypto from 'crypto'
+
+
+function parseRequest() {
+  return function(req, res, next) {
+    req.hull = req.hull || {};
+    rawBody(req, true, (err, body) => {
+      if (err) {
+        return res.handleError('Invalid body', 400);
+      }
+      try {
+        req.hull.body = body;
+        req.hull.message = JSON.parse(body);
+      } catch (parseError) {
+        return res.handleError('Invalid body', 400);
+      }
+      return next();
+    });
+  };
+}
+
+
+function verifySignature(options = {}) {
+  return (req, res, next) => {
+    if (!options.sharedSecret) {
+      return next();
+    }
+    const signature = req.headers['x-signature'];
+
+    if (!signature) {
+      return res.handleError('Missing signature', 403);
+    }
+
+    const digest = crypto
+      .createHmac('sha1', options.sharedSecret)
+      .update(req.hull.body)
+      .digest('hex');
+
+    if (signature == digest) {
+      next()
+    } else {
+      res.handleError('Invalid signature with secret="' + options.sharedSecret + '"', 403);
+
+      next();
+    }
+  }
+}
+
+function enrichWithHullClient() {
+  var _cache = [];
+
+  function getCurrentShip(shipId, client, forceUpdate) {
+    if (forceUpdate) _cache[shipId] = null;
+    _cache[shipId] = _cache[shipId] || client.get(shipId);
+    return _cache[shipId];
+  }
+
+  return function(req, res, next) {
+    const config = ['organization', 'ship', 'secret'].reduce((cfg, k)=> {
+      const val = (req.query[k] || "").trim();
+      if (typeof val === 'string') {
+        cfg[k] = val;
+      } else if (val && val[0] && typeof val[0] === 'string') {
+        cfg[k] = val[0].trim();
+      }
+      return cfg;
+    }, {});
+
+    req.hull = req.hull || {};
+
+    const { message } = req.hull;
+    let forceShipUpdate = false;
+    if (message && message.Subject === 'ship:update') {
+      forceShipUpdate = true;
+    }
+
+    if (config.organization && config.ship && config.secret) {
+      const client = req.hull.client = new Hull({
+        orgUrl: 'https://' + config.organization,
+        platformId: config.ship,
+        platformSecret: config.secret
+      });
+      getCurrentShip(config.ship, client, forceShipUpdate).then((ship) => {
+        req.hull.ship = ship;
+        next();
+      }, (err) => {
+        res.handleError(err.toString(), 400);
+      });
+    } else {
+      next();
+    }
+  };
+}
+
+function processHandlers(handlers) {
+  return function(req, res, next) {
+    try {
+      const eventName = req.hull.message.type
+      const eventHandlers = handlers[eventName];
+      if (eventHandlers && eventHandlers.length > 0) {
+        const context = {
+          hull: req.hull.client,
+          ship: req.hull.ship
+        };
+
+        const processors = eventHandlers.map(fn => fn(req.hull.message, context));
+
+        Promise.all(processors).then(() => {
+          next();
+        }, (err) => {
+          res.handleError('Failed to process message: ' + err.toString(), 500);
+        });
+      } else {
+        next();
+      }
+    } catch ( err ) {
+      res.handleError(err.toString(), 500);
+    }
+  };
+}
+
+
+function errorHandler(onError) {
+  return function(req, res, next) {
+    res.handleError = function(message, status) {
+      if (onError) onError(message, status);
+      res.status(status);
+      res.end(message);
+    };
+    next();
+  };
+}
+
+
+module.exports = function SegmentHandler(options = {}) {
+  const _handlers = {};
+  const app = connect();
+
+  function addEventHandlers(eventsHash) {
+    _.map(eventsHash, (fn, eventName) => addEventHandler(eventName, fn));
+    return this;
+  }
+
+  function addEventHandler(eventName, fn) {
+    _handlers[eventName] = _handlers[eventName] || [];
+    _handlers[eventName].push(fn);
+    return this;
+  }
+
+  if (options.events) {
+    addEventHandlers(options.events);
+  }
+
+  app.use(errorHandler(options.onError));
+  app.use(parseRequest());
+  app.use(verifySignature(options));
+  app.use(enrichWithHullClient());
+  app.use(processHandlers(_handlers));
+  app.use((req, res) => { res.end('ok'); });
+
+  function handler(req, res) {
+    return app.handle(req, res);
+  }
+
+  handler.addEventHandler = addEventHandler;
+
+  return handler;
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,27 @@ import express from 'express';
 import path from 'path';
 import { NotifHandler } from 'hull';
 import devMode from './dev-mode';
+import SegmentHandler from './handler';
+import _ from 'lodash';
+
+const TOP_LEVEL_FIELDS = [
+  'name',
+  'username',
+  'first_name',
+  'last_name',
+  'email',
+  'contact_email',
+  'image',
+  'picture',
+  'phone',
+  'address'
+];
+
+const IGNORED_TRAITS = [
+  'id',
+  'uniqToken',
+  'visitToken'
+];
 
 const notifHandler = NotifHandler({
   onSubscribe: function() {
@@ -9,6 +30,61 @@ const notifHandler = NotifHandler({
   },
   events: {
     'user_report:update': require('./update-user')
+  }
+});
+
+function updateTraits(hull, userId, traits) {
+  return hull.as(userId).traits(traits);
+}
+
+function updateUser(hull, user, asUser) {
+  return hull.as(asUser).put('me', user.properties).then((hullUser) => {
+    return updateTraits(hull, hullUser.id, user.traits);
+  }, (err)=> {
+    if (/[a-z0-9]{24}/i.test(asUser.external_id)) {
+      return updateUser(hull, user, asUser.external_id);
+    } else {
+      throw err;
+    }
+  })
+}
+
+
+const segmentHandler = SegmentHandler({
+
+  sharedSecret: process.env.SECRET,
+
+  onError: function(err) {
+    console.warn("Boom error", err, err.stack);
+  },
+
+  events: {
+    track() {
+      // console.warn("Boom , track!", arguments);
+    },
+    identify({ context, traits, userId }, { ship, hull }) {
+
+      const integrations = (context || {}).integrations || {};
+
+      // Do not resend data to Hull
+      if (integrations.Hull === false) {
+        return false;
+      }
+
+      const user = _.reduce((traits || {}), (u, v, k) => {
+        if (v == null) return u;
+        if (_.include(TOP_LEVEL_FIELDS, k)) {
+          u.properties[k] = v;
+        } else if (!_.include(IGNORED_TRAITS, k)) {
+          u.traits[k] = v;
+        }
+        return u;
+      }, { properties: {}, traits: {} });
+
+      if (userId) {
+        return updateUser(hull, user, { external_id: userId });
+      }
+    }
   }
 });
 
@@ -32,6 +108,7 @@ export default function(port) {
   });
 
   app.post('/notify', notifHandler);
+  app.post('/segment', segmentHandler);
 
   app.listen(port)
 
