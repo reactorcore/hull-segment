a2ba0e2e3588a5197f979290a4f3e62caffcbdc9
Stephane Bellity <sbellity@gmail.com>
2016-04-13 22:26:04 +0200

    better support for tracks

--- server/events/track.js
+++ server/events/track.js
@@ -1,15 +1,22 @@
+import { reduce } from 'lodash';
+
 export default function handleTrack(track, { hull, ship }) {
-  const { context, anonymousId, event, properties, userId, originalTimestamp } = track;
+  const { context, anonymousId, event, properties, userId, originalTimestamp, sentAt, receivedAt } = track;
   const page = (context || {}).page || {};
 
+
+  const created_at = originalTimestamp || sentAt || receivedAt;
+
+  console.warn('raw track', track)
+
   const aId = anonymousId || userId;
-  let sId = (originalTimestamp || new Date().toISOString()).substring(0,10);
+  let sId = (created_at || new Date().toISOString()).substring(0,10);
 
   if (aId) {
     sId = [aId, sId].join('-');
   }
 
-  const payload = {
+  const payload = reduce({
     ip: context.ip || '0',
     _bid: aId,
     _sid: sId,
@@ -19,10 +26,17 @@ export default function handleTrack(track, { hull, ship }) {
     url: page.url,
     useragent: context.userAgent,
     referrer: page.referrer,
-    created_at: originalTimestamp
-  };
+    created_at: created_at
+  }, (p, v, k) => {
+    if (v !== undefined) {
+      p[k] = v;
+    }
+    return p;
+  }, {});
 
   const client = userId ? hull.as({ external_id: userId }) : hull;
+
+  console.warn('track !', payload)
 
   return client.post('t', payload);
 }
