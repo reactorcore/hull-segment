1cc76202ce704fa3cefc2a43c0168c40f9f6176d
Stephane Bellity <sbellity@gmail.com>
2016-04-13 10:51:56 +0200

    fixtures

---
+++ tests/fixtures/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  alias: require('./alias.json'),
+  group: require('./group.json'),
+  identify: require('./identify.json'),
+  page: require('./page.json'),
+  screen: require('./screen.json'),
+  track: require('./track.json')
+};
