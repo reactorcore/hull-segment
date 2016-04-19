1d7cae61b6506c15f91a83b3aac9aca74eda2417
Stephane Bellity <sbellity@gmail.com>
2016-04-19 19:08:40 +0200

    noop handler for screen

--- server/events/index.js
+++ server/events/index.js
@@ -2,6 +2,7 @@ import track from './track'
 import group from './group'
 import alias from './alias'
 import page from './page'
+import screen from './screen'
 import identify from './identify'
 
 export default {
@@ -9,5 +10,6 @@ export default {
   group,
   alias,
   page,
+  screen,
   identify
 }
---
+++ server/events/screen.js
@@ -0,0 +1,1 @@
+export default function handleScreen(screen, { hull, ship }) {}
