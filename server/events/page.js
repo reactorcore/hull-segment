7e109ec34f91da460baadf35a47611ee762eb41b
Romain Dardour <romain@hull.io>
2016-04-28 19:03:26 +0200

    allow .page() and .screen()

--- server/events/page.js
+++ server/events/page.js
@@ -1,1 +1,16 @@
-export default function handlePage(page, { hull, ship }) {}
+import track from './track';
+
+export default function handlePage(payload={}, context) {
+  const { path, search, title } = payload.properties || {}
+  const page = {
+    ...payload,
+    event: 'page',
+    properties: {
+      path,
+      search,
+      title,
+      name: payload.name
+    }
+  }
+  track(page, context);
+}
