e123fdba65aa308512f477ff2a31b1a1d294c5c5
Stephane Bellity <sbellity@gmail.com>
2016-05-17 15:23:35 +0200

    Support email if available to identify user

--- server/scope-hull-client.js
+++ server/scope-hull-client.js
@@ -1,14 +1,22 @@
+const EMAIL_REGEXP = /([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i
+
 export default function(hull, user={}, context={}){
-  const { hullId, userId, anonymousId } = user;
+  const { hullId, userId, anonymousId, traits = {} } = user;
   if(!hullId && !userId && !anonymousId) {
     return hull;
   }
   const as = {};
 
-  if (hullId){ as.id = hullId; }
-  if (userId){ as.external_id = userId; }
-  if (anonymousId){ as.guest_id = anonymousId; }
+  if (hullId || userId) {
+    if (hullId) { as.id = hullId; }
+    if (userId) { as.external_id = userId; }
+  } else if (traits.email && EMAIL_REGEXP.test(traits.email)) {
+    as.email = traits.email.toLowerCase();
+  }
 
+  if (anonymousId) {
+    as.guest_id = anonymousId;
+  }
 
   return hull.as(as);
 }
