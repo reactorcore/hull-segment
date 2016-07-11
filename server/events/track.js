243ec663e9f4fafd55ed96e50a0b9879d9fd3281
Stephane Bellity <sbellity@gmail.com>
2016-07-11 12:23:06 +0200

    Add support for location data

--- server/events/track.js
+++ server/events/track.js
@@ -2,30 +2,27 @@ import { reduce } from "lodash";
 import scoped from "../scope-hull-client";
 
 export default function handleTrack(payload, { hull }) {
-  const { context, anonymousId, event, properties, userId, originalTimestamp, sentAt, receivedAt, integrations = {} } = payload;
+  const { context = {}, anonymousId, event, properties, userId, originalTimestamp, sentAt, receivedAt, integrations = {} } = payload;
 
   const { metric, log } = hull.utils;
-
-  const page = (context || {}).page || {};
+  const { page = {}, location = {}, userAgent, ip = "0" } = context;
+  const { url, referrer } = page;
+  const { latitude, longitude } = location;
 
   const created_at = originalTimestamp || sentAt || receivedAt;
 
-  const aId = anonymousId || userId;
-  let sId = (created_at || new Date().toISOString()).substring(0, 10);
+  const _bid = anonymousId || userId;
+  let _sid = (created_at || new Date().toISOString()).substring(0, 10);
 
-  if (aId) {
-    sId = [aId, sId].join("-");
+  if (_bid) {
+    _sid = [_bid, _sid].join("-");
   }
 
   const trackContext = reduce({
-    ip: context.ip || "0",
-    _bid: aId,
-    _sid: sId,
-    created_at,
     source: "segment",
-    url: page.url,
-    useragent: context.userAgent,
-    referrer: page.referrer
+    created_at, _bid, _sid,
+    url, referrer, useragent: userAgent,
+    ip, latitude, longitude
   }, (p, v, k) => {
     if (v !== undefined) {
       p[k] = v;
