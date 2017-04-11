7de79d5ff1157fcd392eb5f4089622fddd1fc3d8
phillipalexander <git@phillipalexander.io>
2017-04-11 08:43:21 -0700

    Fix syntax

--- server/scope-hull-client.js
+++ server/scope-hull-client.js
@@ -1,6 +1,6 @@
 const EMAIL_REGEXP = /([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i;
 
-export default function scope(hull, user = {} /* , context = {}*/ ) {
+export default function scope(hull, user = {} /* , context = {}*/) {
   const { hullId, userId, anonymousId, traits = {} } = user;
   if (!hullId && !userId && !anonymousId) {
     return hull;
@@ -9,18 +9,18 @@ export default function scope(hull, user = {} /* , context = {}*/ ) {
 
   if (hullId || userId) {
     if (hullId) { as.id = hullId; }
-    //If we have a userId
+    // If we have a userId
     if (userId) {
-      //and it starts with 'auth0'
-      if (userId.indexOf('auth0') === 0) {
-        //then use it as our External ID
-        as.external_id = userId
+      // and it starts with 'auth0'
+      if (userId.indexOf("auth0") === 0) {
+        // then use it as our External ID
+        as.external_id = userId;
       } else {
-        //else drop it in the AnonymousId field
+        // else drop it in the AnonymousId field
         as.guest_id = userId;
       }
     } else if (anonymousId) {
-      //fallback to anonymousId because we don't have a UserId
+      // fallback to anonymousId because we don't have a UserId
       as.guest_id = anonymousId;
     }
   }
