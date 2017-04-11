a785b3c1d055ef79a0e44ca7e3b7117bfea41ee7
phillipalexander <git@phillipalexander.io>
2017-04-10 22:49:40 -0700

    set auth0 id as the primary id for all sources

--- server/scope-hull-client.js
+++ server/scope-hull-client.js
@@ -1,6 +1,6 @@
 const EMAIL_REGEXP = /([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i;
 
-export default function scope(hull, user = {} /* , context = {}*/) {
+export default function scope(hull, user = {} /* , context = {}*/ ) {
   const { hullId, userId, anonymousId, traits = {} } = user;
   if (!hullId && !userId && !anonymousId) {
     return hull;
@@ -9,15 +9,24 @@ export default function scope(hull, user = {} /* , context = {}*/) {
 
   if (hullId || userId) {
     if (hullId) { as.id = hullId; }
-    if (userId) { as.external_id = userId; }
+    //If we have a userId
+    if (userId) {
+      //and it starts with 'auth0'
+      if (userId.indexOf('auth0') === 0) {
+        //then use it as our External ID
+        as.external_id = userId
+      } else {
+        //else drop it in the AnonymousId field
+        as.guest_id = userId;
+      }
+    } else if (anonymousId) {
+      //fallback to anonymousId because we don't have a UserId
+      as.guest_id = anonymousId;
+    }
   }
 
   if (traits.email && EMAIL_REGEXP.test(traits.email)) {
     as.email = traits.email.toLowerCase();
-  }
-
-  if (anonymousId) {
-    as.guest_id = anonymousId;
   }
 
   return hull.as(as);
